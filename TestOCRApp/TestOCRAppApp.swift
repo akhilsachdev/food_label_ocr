ae686b4c2d56dbd00a6ef802a9bbcead7ec5f0c1
Akhil Sachdev <akhilmsachdev@gmail.com>
2023-05-20 17:53:44 -0700

    Initial Commit

---
+++ TestOCRApp/TestOCRAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOCRAppApp.swift
+//  TestOCRApp
+//
+//  Created by Akhil Sachdev on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TestOCRAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
